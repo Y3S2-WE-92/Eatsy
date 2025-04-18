fbd6bc1354bbd55704c92602cec62a03712e17ce
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2025-04-18 21:05:55 +0530

    Implement health check for microservices and create API service endpoints

--- web-client/src/App.jsx
+++ web-client/src/App.jsx
@@ -1,35 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { apiEndpoints } from "./services/api-services";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [restaurantsAPIhealth, setRestaurantsAPIhealth] = useState(null);
+  const [deliveryAPIhealth, setDeliveryAPIhealth] = useState(null);
+  const [ordersAPIhealth, setOrdersAPIhealth] = useState(null);
+  const [paymentAPIhealth, setPaymentAPIhealth] = useState(null);
+  const [notificationsAPIhealth, setNotificationsAPIhealth] = useState(null);
+
+  const fetchHealthStatus = async () => {
+    try {
+      const response = await fetch(apiEndpoints.RestaurantsAPIhealth);
+      const data = await response.json();
+      setRestaurantsAPIhealth(data.status);
+    } catch (error) {
+      console.error("Error fetching Restaurants API health:", error);
+    }
+  };
+  const fetchDeliveryAPIhealth = async () => {
+    try {
+      const response = await fetch(apiEndpoints.DeliveryAPIhealth);
+      const data = await response.json();
+      setDeliveryAPIhealth(data.status);
+    } catch (error) {
+      console.error("Error fetching Delivery API health:", error);
+    }
+  };
+  const fetchOrdersAPIhealth = async () => {
+    try {
+      const response = await fetch(apiEndpoints.OrdersAPIhealth);
+      const data = await response.json();
+      setOrdersAPIhealth(data.status);
+    } catch (error) {
+      console.error("Error fetching Orders API health:", error);
+    }
+  };
+  const fetchPaymentAPIhealth = async () => {
+    try {
+      const response = await fetch(apiEndpoints.PaymentAPIhealth);
+      const data = await response.json();
+      setPaymentAPIhealth(data.status);
+    } catch (error) {
+      console.error("Error fetching Payment API health:", error);
+    }
+  };
+  const fetchNotificationsAPIhealth = async () => {
+    try {
+      const response = await fetch(apiEndpoints.NotificationsAPIhealth);
+      const data = await response.json();
+      setNotificationsAPIhealth(data.status);
+    } catch (error) {
+      console.error("Error fetching Notifications API health:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchHealthStatus();
+    fetchDeliveryAPIhealth();
+    fetchOrdersAPIhealth();
+    fetchPaymentAPIhealth();
+    fetchNotificationsAPIhealth();
+  }, []);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>
+          Eatsy - Cloud-Native Food Ordering & Delivery System using
+          Microservices
+        </h1>
+
+        <ul>
+          <li>
+            <h2>Restaurants API - Status: {restaurantsAPIhealth}</h2>
+          </li>
+          <li>
+            <h2>Delivery API - Status: {deliveryAPIhealth}</h2>
+          </li>
+          <li>
+            <h2>Orders API - Status: {ordersAPIhealth}</h2>
+          </li>
+          <li>
+            <h2>Payment API - Status: {paymentAPIhealth}</h2>
+          </li>
+          <li>
+            <h2>Notifications API - Status: {notificationsAPIhealth}</h2>
+          </li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
