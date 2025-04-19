e19a3d1cfbad390d9cafc38ab1308515da48692b
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2025-04-20 00:53:39 +0530

    Add CloseButton, ThemeButton, ThemeLogo components and Footer; update index.js exports

---
+++ admin-panel/src/components/index.js
@@ -0,0 +1,17 @@
+import ThemeLogo from "./Logos/ThemeLogo";
+import ThemeButton from "./Buttons/ThemeButton";
+import CloseButton from "./Buttons/CloseButton";
+
+import Footer from "./Footer/Footer";
+
+export {
+  //Logos
+  ThemeLogo,
+  
+  //Buttons
+  ThemeButton,
+  CloseButton,
+
+  //Common
+  Footer,
+};
