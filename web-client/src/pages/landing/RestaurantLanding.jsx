4dab4041725a5ad69cd0f191cf5dddab6ddfd66f
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2025-04-19 19:59:47 +0530

    Implement RestaurantLanding component with Navbar and introductory content

--- web-client/src/pages/landing/RestaurantLanding.jsx
+++ web-client/src/pages/landing/RestaurantLanding.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { RestaurantLandingNavbar } from '../../components'
 
 function RestaurantLanding() {
   return (
-    <div>RestaurantLanding</div>
+    <div>
+        <RestaurantLandingNavbar />
+        <div className="relative flex flex-col items-center justify-center">
+            <h1 className="text-4xl font-bold mt-8">Eatsy for Restaurants</h1>
+            <p className="mt-4">Manage your restaurant with ease.</p>
+            <p className="mt-2">Sign up to get started!</p>
+        </div>
+    </div>
   )
 }
 
