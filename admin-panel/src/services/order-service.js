68daead0b4150d47ade3a85ff8fb560d6092b4e2
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2025-04-20 02:05:00 +0530

    Add service files for delivery, notification, order, payment, restaurant, and user APIs

---
+++ admin-panel/src/services/order-service.js
@@ -0,0 +1,9 @@
+// local development - Docker
+const ORDERS_API_URL = "http://localhost:4003/api";
+
+// local development
+// const ORDERS_API_URL = "http://localhost:3000/api";
+
+export const orderAPI = {
+    OrdersAPIhealth: `${ORDERS_API_URL}/health`,
+};
