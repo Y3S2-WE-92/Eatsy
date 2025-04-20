adbe6db99c6f859c3d6b8b953e90d98471960fe2
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2025-04-21 00:01:08 +0530

    Implement DeliveryLayout with DeliveryNavBar and add Home component

--- web-client/src/layout/user-layouts/DeliveryLayout.jsx
+++ web-client/src/layout/user-layouts/DeliveryLayout.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { DeliveryNavBar } from "../../components";
+
+import Home from "../../pages/delivery/Home";
 
 function DeliveryLayout() {
+  const sections = [
+    { name: "Home", path: "/delivery/" },
+    { name: "Orders", path: "/delivery/orders" },
+  ]
+
   return (
-    <div>DeliveryLayout</div>
-  )
+    <div>
+      <DeliveryNavBar sections={sections} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/orders" element={<div>Orders</div>} />
+      </Routes>
+    </div>
+  );
 }
 
-export default DeliveryLayout
+export default DeliveryLayout;
