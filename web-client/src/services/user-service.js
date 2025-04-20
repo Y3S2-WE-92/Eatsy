a987b0ff427564b7f00096104073bf81643c1fd4
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2025-04-20 15:01:50 +0530

    Refactor user service API endpoints for admin and customer authentication

--- web-client/src/services/user-service.js
+++ web-client/src/services/user-service.js
@@ -8,6 +8,12 @@ export const userAPI = {
     UserAPIhealth: `${USER_API_URL}/health`,
 
     //Authentication
-    UserRegister: `${USER_API_URL}/auth/register`,
-    UserLogin: `${USER_API_URL}/auth/login`,
+    CustomerRegister: `${USER_API_URL}/register/customer`,
+    DeliveryRegister: `${USER_API_URL}/register/delivery`,
+    RestaurantRegister: `${USER_API_URL}/register/restaurant`,
+
+    //Login
+    CustomerLogin: `${USER_API_URL}/auth/customer`,
+    DeliveryLogin: `${USER_API_URL}/auth/delivery`,
+    RestaurantLogin: `${USER_API_URL}/auth/restaurant`,
 }
