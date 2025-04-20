d5aa1f16b7200d6147614b857bb096f9d413ed44
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2025-04-20 15:36:09 +0530

    Add login functionality with form handling and API integration; update user service API URL

--- admin-panel/src/pages/Login.jsx
+++ admin-panel/src/pages/Login.jsx
@@ -1,15 +1,71 @@
-import React from 'react'
-import { ThemeButton, ThemeLogo } from '../components'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { ThemeButton, ThemeLogo } from "../components";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { userAPI } from "../services";
+import { jwtDecode } from "jwt-decode";
 
 function Login() {
   const navigate = useNavigate();
+  
+  const [loginData, setLoginData] = useState({
+    username: "",
+    password: "",
+  });
 
-  const handleLogin = (e) => {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setLoginData({
+      ...loginData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Logic to handle login
-    navigate('/admin-panel/');
-  }
+
+    if (!loginData.username || !loginData.password) {
+      alert("Please fill all fields");
+      return;
+    }
+
+    try {
+      const response = await axios.post(userAPI.AdminLogin, {
+        username: loginData.username,
+        password: loginData.password,
+      });
+
+      if (response.status === 200) {
+        const { token } = response.data;
+        if (!token) {
+          throw new Error("No token received from server");
+        }
+
+        const decodedToken = jwtDecode(token);
+        // console.log("Decoded JWT:", decodedToken);
+
+        localStorage.setItem("token", token);
+
+        localStorage.setItem(
+          "user",
+          JSON.stringify({
+            id: decodedToken.id,
+            name: response.data.user.name,
+            username: response.data.user.username,
+          })
+        );
+
+        console.log("Login successful", response.data);
+        alert("Login successful!");
+        navigate("/admin-panel");
+      }
+    } catch (error) {
+      console.error("Error logging in:", error);
+      alert(
+        error.response?.data?.message || "Error logging in. Please try again."
+      );
+    }
+  };
 
   return (
     <div className="login min-h-screen flex items-center justify-center">
@@ -20,24 +76,30 @@ function Login() {
       <div className="card card-xl bg-base-300 shadow-sm w-full max-w-sm mx-auto">
         <div className="card-body">
           <ThemeLogo style={"w-48 mx-auto"} />
-          <div className="text-xl mt-4 text-center font-bold mb-4">Admin Panel</div>
-          <form
-            className="form-control flex flex-col gap-2"
-          >
+          <div className="text-xl mt-4 text-center font-bold mb-4">
+            Admin Panel
+          </div>
+          <form onSubmit={handleSubmit} className="form-control flex flex-col gap-2">
             <input
               type="text"
               name="username"
               placeholder="Username"
               className="input"
+              onChange={handleChange}
+              value={loginData.username}
             />
             <input
               type="password"
               name="password"
               placeholder="Password"
               className="input"
+              onChange={handleChange}
+              value={loginData.password}
             />
             <div className="card-actions justify-end mt-3">
-              <button onClick={handleLogin} type="button" className="btn btn-primary w-full">
+              <button
+                className="btn btn-primary w-full"
+              >
                 Login
               </button>
             </div>
@@ -45,7 +107,7 @@ function Login() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
