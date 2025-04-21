1e662aafafb911fcc07467a5bc1bb910163f37de
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2025-04-21 13:24:53 +0530

    Implement CustomerLayout with routing and add Home component

--- web-client/src/layout/user-layouts/CustomerLayout.jsx
+++ web-client/src/layout/user-layouts/CustomerLayout.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import { CustomerNavBar } from '../../components';
+
+import Home from '../../pages/restaurant/Home';
 
 function CustomerLayout() {
+  const sections = [
+    { name: "Home", path: "/customer/" },
+    { name: "My Orders", path: "/customer/my-orders" },
+  ]
+
   return (
-    <div>CustomerLayout</div>
+    <div>
+      <CustomerNavBar sections={sections} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/my-orders" element={<div>My Orders</div>} />
+      </Routes>
+    </div>
   )
 }
 
