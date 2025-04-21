73c3054df7bf15b33f926e0530a18cc5933a9fe7
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2025-04-21 13:06:53 +0530

    Implement RestaurantLayout and Home components with routing

--- web-client/src/layout/user-layouts/RestaurantLayout.jsx
+++ web-client/src/layout/user-layouts/RestaurantLayout.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { RestaurantNavbar } from "../../components";
+
+import Home from "../../pages/restaurant/Home";
 
 function RestaurantLayout() {
+  const sections = [
+    { name: "Home", path: "/restaurant/" },
+    { name: "Orders", path: "/restaurant/orders" },
+    { name: "My Menus", path: "/restaurant/menu" },
+  ];
+
   return (
-    <div>RestaurantLayout</div>
-  )
+    <div>
+      <RestaurantNavbar sections={sections} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/orders" element={<div>Orders</div>} />
+        <Route path="/menu" element={<div>Menu</div>} />
+      </Routes>
+    </div>
+  );
 }
 
-export default RestaurantLayout
+export default RestaurantLayout;
