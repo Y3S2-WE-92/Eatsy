a7c22a3ad763e15951c2462f3ad2e6636ef48b72
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2025-04-22 20:20:05 +0530

    Add Checkout component with PageTitle for checkout page navigation

---
+++ web-client/src/pages/customer/Checkout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { PageTitle } from '../../components'
+
+function Checkout() {
+  return (
+    <div>
+      <PageTitle title='Checkout' backLink='/customer' />
+    </div>
+  )
+}
+
+export default Checkout
