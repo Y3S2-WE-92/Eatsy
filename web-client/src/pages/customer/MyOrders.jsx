6f8ee7d5d2a957ca5f8064657ea9f2e05bb64f7d
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2025-04-22 22:06:49 +0530

    Add ShoppingCartButton to MyOrders component

--- web-client/src/pages/customer/MyOrders.jsx
+++ web-client/src/pages/customer/MyOrders.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { PageTitle } from "../../components";
 import { styles } from "../../styles/styles";
+import {ShoppingCartButton} from "../../components";
 
 function MyOrders() {
   return (
     <div className={`${styles.paddingX} flex flex-col`}>
+      <ShoppingCartButton />
       <PageTitle title="My Orders" backLink="/customer" />
       <div className="card bg-base-300">
         <div className="card-body"></div>
