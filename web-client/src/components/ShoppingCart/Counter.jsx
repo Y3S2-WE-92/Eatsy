f3c7ddc35c20c7854ec7998541b86aa31d75225e
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2025-04-22 12:03:49 +0530

    Add existing quantity prop to Counter component and implement effect for initial count

--- web-client/src/components/ShoppingCart/Counter.jsx
+++ web-client/src/components/ShoppingCart/Counter.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FaPlus, FaMinus } from "react-icons/fa";
 
-function Counter() {
+function Counter({existingQty=0}) {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setCount(existingQty);
+  }, [existingQty]);
 
   const handleMinusClick = () => {
     setCount(count - 1);
@@ -14,7 +18,7 @@ function Counter() {
 
   return (
     <div className="flex flex-row items-center gap-2 w-full justify-center">
-      <button className="btn btn-primary btn-circle btn-xs" onClick={handleMinusClick}>
+      <button className="btn btn-primary btn-circle btn-xs" onClick={handleMinusClick} disabled={count === 0}>
         <FaMinus />
       </button>
       <input
