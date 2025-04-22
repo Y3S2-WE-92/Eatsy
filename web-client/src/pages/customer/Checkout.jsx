aa7d583e5845a494131d8e67e791f0140849c66e
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2025-04-22 21:26:52 +0530

    Add styles to Checkout component for improved layout

--- web-client/src/pages/customer/Checkout.jsx
+++ web-client/src/pages/customer/Checkout.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { PageTitle } from '../../components'
+import { styles } from '../../styles/styles'
 
 function Checkout() {
   return (
-    <div>
+    <div className={`${styles.paddingX} flex flex-col`}>
       <PageTitle title='Checkout' backLink='/customer' />
     </div>
   )
