164452b1ef166b67b685e99de1d1947811317d16
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2025-04-22 21:26:02 +0530

    Refactor PageTitle component to remove unused styles import and simplify padding classes

--- web-client/src/components/PageComponents/PageTitle.jsx
+++ web-client/src/components/PageComponents/PageTitle.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import BackButton from '../Buttons/BackButton'
-import { styles } from '../../styles/styles'
 
 function PageTitle({title="", backLink=""}) {
   return (
-    <div className={`${styles.paddingX} pt-8 flex flex-row items-center gap-4 w-full`}>
+    <div className={`py-4 md:py-8 flex flex-row items-center gap-4 w-full`}>
         <BackButton link={backLink} />
         <h1 className='text-3xl font-bold'>{title}</h1>
     </div>
