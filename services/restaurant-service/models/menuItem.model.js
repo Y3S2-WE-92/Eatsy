c4953db6d373d90f89b772e12c7e50659da46132
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2025-04-24 01:36:20 +0530

    restaurant-service models created

---
+++ services/restaurant-service/models/menuItem.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const MenuItemSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    description: String,
+    category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+    availability: { type: Boolean, default: true },
+    estPreperationTime: Number,
+    restaurantId: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("MenuItem", MenuItemSchema);
