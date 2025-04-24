4af8d61941cc2b00062098bdf8713cac93b9404a
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2025-04-25 02:30:19 +0530

    Refactor menu item controller for improved code formatting

--- services/restaurant-service/controllers/menuItem.controller.js
+++ services/restaurant-service/controllers/menuItem.controller.js
@@ -3,47 +3,53 @@ const Category = require("../models/category.model");
 const { getRestaurantById } = require("../utils/user-service.util");
 
 const getAllMenuItems = async (req, res) => {
-    try {
-        const menuItems = await MenuItem.find().populate("category");
-        res.json(menuItems);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+  try {
+    const menuItems = await MenuItem.find().populate("category");
+    res.json(menuItems);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const createMenuItem = async (req, res) => {
-    try {
-        const category = await Category.findById(req.body.category);
-        if (!category) {
-            return res.status(404).json({ message: "Category not found" });
-        }
-        const menuItem = await MenuItem.create(req.body);
-        res.status(201).json(menuItem);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+  try {
+    const category = await Category.findById(req.body.category);
+    if (!category) {
+      return res.status(404).json({ message: "Category not found" });
     }
+    const menuItem = await MenuItem.create(req.body);
+    res.status(201).json(menuItem);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const getMenuItemByID = async (req, res) => {
-    try {
-        const menuItem = await MenuItem.findById(req.params.id).populate("category");
-        res.json(menuItem);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-}
+  try {
+    const menuItem = await MenuItem.findById(req.params.id).populate(
+      "category"
+    );
+    res.json(menuItem);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const getMenuItemsByRestaurantID = async (req, res) => {
-    try {
-        const menuItems = await MenuItem.find({ restaurantID: req.params.id }).populate("category");
+  try {
+    const menuItems = await MenuItem.find({
+      restaurantID: req.params.id,
+    }).populate("category");
 
-        const restaurant = await getRestaurantById(menuItems.restaurantID);
+    res.json({ menuItems });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-        if (!restaurant) return res.status(404).json({ message: 'Restaurant not found' });
-        res.json(menuItems, restaurant);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-}
-
-module.exports = { getAllMenuItems, createMenuItem, getMenuItemByID, getMenuItemsByRestaurantID };
+module.exports = {
+  getAllMenuItems,
+  createMenuItem,
+  getMenuItemByID,
+  getMenuItemsByRestaurantID,
+};
