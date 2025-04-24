9993021283e4c48df83d714efabbba776a670113
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2025-04-24 10:52:27 +0530

    Fix typo in mongoose import statement in CustomerLocation model

--- services/user-service/models/customer/customerLocation.model.js
+++ services/user-service/models/customer/customerLocation.model.js
@@ -1,15 +1,18 @@
-const mongoose = required("mongoose");
+const mongoose = require("mongoose");
 
-const CustomerLocationSchema = new mongoose.Schema({
-  customerID: { type: mongoose.Schema.Types.ObjectId, ref: "Customer" },
-  name: { type: String, required: true },
-  deliveryAddress: {
-    address: { type: String, required: true },
-    location: {
-      type: { type: String, enum: ["Point"], default: "Point" },
-      coordinates: { type: [Number], required: true },
+const CustomerLocationSchema = new mongoose.Schema(
+  {
+    customerID: { type: mongoose.Schema.Types.ObjectId, ref: "Customer" },
+    name: { type: String, required: true },
+    deliveryAddress: {
+      address: { type: String, required: true },
+      location: {
+        type: { type: String, enum: ["Point"], default: "Point" },
+        coordinates: { type: [Number], required: true },
+      },
     },
   },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("CustomerLocation", CustomerLocationSchema);
