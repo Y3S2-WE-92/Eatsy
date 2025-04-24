c611823d5e33ae07b1bf64ed40570d5a88eea159
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2025-04-24 14:43:51 +0530

    Refactor admin and restaurant API endpoints in frontends

--- admin-panel/src/services/user-service.js
+++ admin-panel/src/services/user-service.js
@@ -1,11 +1,12 @@
+import { getAllAdmins } from "../../../services/user-service/controllers/admin/admin.controller";
+
 const USER_API_URL = import.meta.env.VITE_USER_API_URL;
 
 export const userAPI = {
     UserAPIhealth: `${USER_API_URL}/health`,
 
-    //Authentication
-    AdminRegister: `${USER_API_URL}/register/admin`,
-
-    //Login
-    AdminLogin: `${USER_API_URL}/auth/admin`,
+    AdminRegister: `${USER_API_URL}/admin/register`,
+    AdminLogin: `${USER_API_URL}/admin/login`,
+    getAllAdmins: `${USER_API_URL}/admin`,
+    getAdminByID: `${USER_API_URL}/admin/:id`,
 }
