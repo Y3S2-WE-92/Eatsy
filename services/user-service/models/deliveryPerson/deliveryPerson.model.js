ef5cf3de69855f27530446b7acbe9dcac2fc5ccc
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2025-04-24 10:53:46 +0530

    Add DeliveryPerson model with schema

---
+++ services/user-service/models/deliveryPerson/deliveryPerson.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const DeliveryPersonSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: { type: String, unique: true },
+    phone: { type: String, unique: true },
+    username: { type: String, unique: true },
+    password: { type: String, required: true },
+    profileImage: { type: String },
+    nic: { type: String, required: true },
+    vehicleNo: { type: String, required: true },
+    licenseNo: { type: String, required: true },
+    availability: { type: Boolean, default: false },
+    verifiedBy: { type: mongoose.Schema.Types.ObjectId, ref: "Admin" },
+    accountStatus: { type: String, default: "pending" },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("DeliveryPerson", DeliveryPersonSchema);
