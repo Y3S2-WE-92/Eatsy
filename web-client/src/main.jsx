f0fc19ef0473337479ea1cd9c37c8892d6620dfc
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2025-04-25 13:12:48 +0530

    Add Redux store and slices for customer and cart management

--- web-client/src/main.jsx
+++ web-client/src/main.jsx
@@ -1,13 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
 import "./index.css";
 import AppLayout from "./layout/AppLayout.jsx";
 import { ThemeProvider } from "./theme/ThemeContext.jsx";
 
+store.subscribe(() => {
+  console.log('Redux state changed:', store.getState());
+});
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider>
-      <AppLayout />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider>
+        <AppLayout />
+      </ThemeProvider>
+    </Provider>
   </StrictMode>
 );
