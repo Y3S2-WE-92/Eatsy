1cc1711bf7b8aa2deaaab6bc44d9b69bafd74f65
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2025-04-26 00:57:52 +0530

    Added Add to Cart Button disabled condition

--- web-client/src/components/Modals/Customer/FoodItemModal.jsx
+++ web-client/src/components/Modals/Customer/FoodItemModal.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Counter from "../../ShoppingCart/Counter";
 import { formatCurrency } from "../../../utils/format-utils/CurrencyUtil";
 import ImageLoader from "../../Loaders/ImageLoader";
@@ -16,13 +16,13 @@ function FoodItemModal({ item, isOpen, onClose }) {
   const [quantities, setQuantities] = useState({});
 
   const handleQuantityChange = (sizeID, qty) => {
-    setQuantities(prev => ({ ...prev, [sizeID]: qty }));
+    setQuantities((prev) => ({ ...prev, [sizeID]: qty }));
   };
 
   const handleAddToCart = () => {
     const cartItems = item.sizes
-      .filter(size => quantities[size._id] > 0)
-      .map(size => ({
+      .filter((size) => quantities[size._id] > 0)
+      .map((size) => ({
         itemID: item._id,
         name: item.name,
         selectedSize: size.size,
@@ -31,12 +31,14 @@ function FoodItemModal({ item, isOpen, onClose }) {
       }));
 
     if (cartItems.length > 0) {
-      dispatch(addToCart({
-        restaurantID: item.restaurantID,
-        items: cartItems,
-      }));
+      dispatch(
+        addToCart({
+          restaurantID: item.restaurantID,
+          items: cartItems,
+        })
+      );
     }
-    toast.success("Item added to cart");
+    toast.success("Items added to your cart");
     onClose();
   };
 
@@ -68,7 +70,10 @@ function FoodItemModal({ item, isOpen, onClose }) {
                 <div className="flex flex-col gap-4">
                   <h2 className="card-title">{item.name}</h2>
                   <p>{item.description}</p>
-                  <p>Est. Preparation Time: {formatMinutesTime(item.estPreperationTime)}</p>
+                  <p>
+                    Est. Preparation Time:{" "}
+                    {formatMinutesTime(item.estPreperationTime)}
+                  </p>
                   <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
                     <table className="table">
                       <tbody>
@@ -77,10 +82,12 @@ function FoodItemModal({ item, isOpen, onClose }) {
                             <td>{size.size}</td>
                             <td>{formatCurrency(size.price)}</td>
                             <td>
-                            <Counter
-                            existingQty={quantities[size._id] || 0}
-                            onQuantityChange={(qty) => handleQuantityChange(size._id, qty)}
-                          />
+                              <Counter
+                                existingQty={quantities[size._id] || 0}
+                                onQuantityChange={(qty) =>
+                                  handleQuantityChange(size._id, qty)
+                                }
+                              />
                             </td>
                           </tr>
                         ))}
@@ -92,7 +99,15 @@ function FoodItemModal({ item, isOpen, onClose }) {
                       <button className="btn btn-outline" onClick={onClose}>
                         Close
                       </button>
-                      <button className="btn btn-primary" onClick={handleAddToCart}>Add to Cart</button>
+                      <button
+                        className="btn btn-primary"
+                        onClick={handleAddToCart}
+                        disabled={Object.values(quantities).every(
+                          (qty) => qty === 0 || qty === undefined
+                        )}
+                      >
+                        Add to Cart
+                      </button>
                     </div>
                   </div>
                 </div>
