445b744caf13083af391510b20a29590b51d3da6
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2025-04-25 22:44:22 +0530

    Add utility function to format minutes into hours and minutes

---
+++ web-client/src/utils/format-utils/TimeFormatUtil.js
@@ -0,0 +1,11 @@
+// Input in minutes, formats in to hours and minutes
+
+export const formatMinutesTime = (time) => {
+    const hours = Math.floor(time / 60);
+    if (hours === 0) {
+        return `${time} min`;
+    }else {
+        const minutes = time % 60;
+        return `${hours} h ${minutes} min`;
+    }
+};
