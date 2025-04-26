3dcb305df1207850ecb3c60545f8768efa76a9f4
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2025-04-26 22:50:07 +0530

    Remove socket logic and order handling from Orders component

--- web-client/src/pages/restaurant/Orders.jsx
+++ web-client/src/pages/restaurant/Orders.jsx
@@ -1,46 +1,9 @@
-import React, { useState, useEffect } from "react";
-import {useSelector} from "react-redux";
-
-import {
-  connectRestaurantSocket,
-  listenNewOrder,
-  disconnectSocket,
-} from "../../sockets/restaurant.socket";
+import React from 'react'
 
 function Orders() {
-  const [orders, setOrders] = useState([]);
-  const restaurantID = useSelector((state) => state.restaurant.loginRestaurant?.id);
-
-  const user = JSON.parse(localStorage.getItem("user"));
-
-  useEffect(() => {
-    connectRestaurantSocket(restaurantID);
-
-    // 2. Listen for new orders
-    listenNewOrder((newOrder) => {
-      setOrders((prev) => [...prev, newOrder]);
-      alert("You have a new order!");
-    });
-
-    // 3. Disconnect on unmount
-    return () => {
-      disconnectSocket();
-    };
-  }, [restaurantID]);
-  
   return (
-    <div>
-      <h2>Restaurant Dashboard</h2>
-      <h3>New Orders:</h3>
-      <ul>
-        {orders.map((order) => (
-          <li key={order._id}>
-            {order.refNo} - {order.status}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    <div>Orders</div>
+  )
 }
 
-export default Orders;
+export default Orders
