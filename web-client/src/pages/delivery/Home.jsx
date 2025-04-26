9d39971984c902b5aa1c14682167b3b528dda87a
Dilina Jayalath <dspjayalath@gmail.com>
2025-04-27 02:56:11 +0530

    added new UI changes

--- web-client/src/pages/delivery/Home.jsx
+++ web-client/src/pages/delivery/Home.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { DeliveryMap } from "../../components";
-import { orderDetails } from "../../constants";
 
 function Home() {
   return (
     <>
       <div>Home</div>
-      <DeliveryMap mode="delivery" orderData={orderDetails}/>
+      <DeliveryMap mode="delivery"/>
     </>
   );
 }
