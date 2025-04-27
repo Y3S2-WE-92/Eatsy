c50e02bfbfdf0d45e921c1952bf3efc308ee7952
Sandali Dias <ssd.dias29@gmail.com>
2025-04-27 15:23:57 +0530

    Refactor sendPaybackNotification inside a util folder

--- services/payment-service/controllers/payback.controller.js
+++ services/payment-service/controllers/payback.controller.js
@@ -1,27 +1,6 @@
 const Payback = require("../models/payback.model");
 const CommissionSetting = require("../models/commissionSetting.model");
-const axios = require("axios");
-
-const NOTIFICATION_SERVICE_URL = process.env.NOTIFICATION_SERVICE_URL || "http://localhost:4005/api/notifications/send";
-
-// Helper function for sending mails
-async function sendPaybackNotification({ to, subject, text, html }) {
-    try {
-        await axios.post(NOTIFICATION_SERVICE_URL, {
-            to,
-            subject,
-            text,
-            html,
-            metadata: {
-                service: "payment-service",
-                type: "payback-created"
-            }
-        });
-        console.log(`Notification sent to ${to}`);
-    } catch (error) {
-        console.error(`Failed to send notification to ${to}`, error.message);
-    }
-}
+const sendPaybackNotification = require("../utils/sendPaybackNotification.util");
 
 const createPayback = async (req, res) => {
     try {
