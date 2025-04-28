ab11f0fed440758b01a7d22b010c353759c0d231
Sandali Dias <ssd.dias29@gmail.com>
2025-04-29 04:40:06 +0530

    Payback page implementation in restaurant and delivery

--- services/payment-service/models/payback.model.js
+++ services/payment-service/models/payback.model.js
@@ -5,7 +5,9 @@ const paybackSchema = new mongoose.Schema({
   receiverId: { type: String, required: true },
   receiverType: { type: String, required: true , enum: ['restaurant', 'delivery']},
   amountReceived: { type: Number, required: true },
-  platformCommission: { type: Number, required: true }
+  platformCommission: { type: Number, required: true },
+  status: {type: String, required: true , enum: ['pending', 'completed'], default: 'pending'},
+  date: {type: Date, default: Date.now()}
 }, { timestamps: true });
 
 const Payback = mongoose.model("Payback", paybackSchema);
--- web-client/src/layout/user-layouts/RestaurantLayout.jsx
+++ web-client/src/layout/user-layouts/RestaurantLayout.jsx
@@ -5,12 +5,14 @@ import { RestaurantNavbar } from "../../components";
 import Home from "../../pages/restaurant/Home";
 import MyMenus from "../../pages/restaurant/MyMenus";
 import Orders from "../../pages/restaurant/Orders";
+import Earnings from '../../pages/restaurant/Earnings'
 
 function RestaurantLayout() {
   const sections = [
     { name: "Home", path: "/restaurant/" },
     { name: "Orders", path: "/restaurant/orders" },
     { name: "My Menus", path: "/restaurant/menu" },
+    { name: "Earnings", path: "/restaurant/earnings" },
   ];
 
   return (
@@ -20,6 +22,7 @@ function RestaurantLayout() {
         <Route path="/" element={<Home />} />
         <Route path="/orders" element={<Orders/>} />
         <Route path="/menu" element={<MyMenus/>} />
+        <Route path="/earnings" element={<Earnings/>} />
       </Routes>
     </div>
   );
